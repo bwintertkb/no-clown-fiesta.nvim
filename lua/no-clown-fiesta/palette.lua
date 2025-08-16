6af159d2146d74f54e1ae35d3bc768682a83a954
bwintertkb <bartekwinter3@gmail.com>
2025-08-16 10:58:03 +0100

    Changed fg colour

--- lua/no-clown-fiesta/palette.lua
+++ lua/no-clown-fiesta/palette.lua
@@ -1,11 +1,11 @@
 local colors = {
   none = "NONE",
-  fg = "#EFD880",
+  fg = "#FFF176",
   bg = "#151515",
   alt_bg = "#171717",
   accent = "#202020",
   -- white = "#E1E1E1",
-  white = "#EFD880",
+  white = "#FFF176",
   gray = "#373737",
   medium_gray = "#727272",
   light_gray = "#AFAFAF",
