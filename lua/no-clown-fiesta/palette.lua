f241907cff05a7ddc9c4e427835e7eb0bbdb5f20
bwintertkb <bartekwinter3@gmail.com>
2025-08-16 11:04:07 +0100

    Changed fg colour

--- lua/no-clown-fiesta/palette.lua
+++ lua/no-clown-fiesta/palette.lua
@@ -1,11 +1,11 @@
 local colors = {
   none = "NONE",
-  fg = "#C2BC8E",
+  fg = "#FFF8C4",
   bg = "#151515",
   alt_bg = "#171717",
   accent = "#202020",
   -- white = "#E1E1E1",
-  white = "#C2BC8E",
+  white = "#FFF8C4",
   gray = "#373737",
   medium_gray = "#727272",
   light_gray = "#AFAFAF",
